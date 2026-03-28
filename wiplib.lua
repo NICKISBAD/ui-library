5c2d6dfa07e5370e1f4e1ca8aa81da34ea9b0ac4
annxxs <107813557+NICKISBAD@users.noreply.github.com>
2026-03-28 12:31:56 +0500

    Fix function definition syntax in wiplib.lua

--- wiplib.lua
+++ wiplib.lua
@@ -1,4 +1,4 @@
-repeat task.wait() until game:IsLoaded()
+function task.wait() until game:IsLoaded()
 
 local UIS = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
@@ -218,7 +218,7 @@ end
 
 -- NOTIFICATIONS
 local notifications = {}
-local function Notify(text,color)
+function Notify(text,color)
 	color = color or Themes[Theme].Accent
 	local notif = UI:Create("Frame", {Size=UDim2.new(0,220,0,40), BackgroundColor3=Themes[Theme].Card, AnchorPoint=Vector2.new(1,1), Position=UDim2.new(1,20,1,20), ZIndex=10}, ScreenGui)
 	UI:Corner(notif, 8)
